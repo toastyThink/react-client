ce3ced972d5c7bfc4b089db8331e42228434be37
Brendan <shaefferbrendan@gmail.com>
2024-01-21 19:47:34 -0500

    adjusted routing name link for favorites

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -62,7 +62,7 @@ const Header = () => {
                     <div className="flex flex-shrink-0 items-center">
                       <img
                         className="h-8 w-auto"
-                        src="/src/assets/logo.svg"
+                        src="./src/assets/logo.svg"
                         alt="Your Company"
                       />
 
